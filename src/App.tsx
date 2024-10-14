11c80d7742bf80c99587a1231197c57c837f13f4
Christian DerManuelian <christian.dermanuelian@gmail.com>
2024-10-13 19:57:32 -0700

    Set Background Video

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import background_loop from "./assets/videos/background_loop.mp4";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <video autoPlay muted loop id="bg-video">
+        <source src={background_loop} type="video/mp4" />
+      </video>
+    </>
+  );
 }
 
 export default App;
