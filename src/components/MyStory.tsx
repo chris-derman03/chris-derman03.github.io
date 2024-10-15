3b379341f60a3896c059034af5c7bf66c83e0f1e
Christian DerManuelian <christian.dermanuelian@gmail.com>
2024-10-14 20:42:57 -0700

    Refactored font size to work globally with all components. Fixed some minor oopsy doopsies on the story component

--- src/components/MyStory.tsx
+++ src/components/MyStory.tsx
@@ -1,10 +1,4 @@
-import {
-  useCallback,
-  useEffect,
-  useLayoutEffect,
-  useRef,
-  useState,
-} from "react";
+import { useEffect, useRef, useState } from "react";
 import { Box, Flex, Grid, GridItem, HStack } from "@chakra-ui/react";
 import "./BodySections.css";
 import story from "../data/story.json";
@@ -25,7 +19,7 @@ const MyStory = () => {
   useEffect(() => {
     const timeoutId = setTimeout(() => {
       updateHeight(); // Set initial height after timeout
-    }, 0); // 0 ms timeout to allow the DOM to finish rendering
+    }, 10);
 
     window.addEventListener("resize", updateHeight); // Update height on resize
 
@@ -33,21 +27,26 @@ const MyStory = () => {
       clearTimeout(timeoutId); // Cleanup timeout
       window.removeEventListener("resize", updateHeight); // Cleanup resize event
     };
-  }, []); // Empty dependency array to run once on mount
+  }, []);
 
   return (
     <div className="bodySection" id="myStory">
-      <Grid templateColumns="97% 3%" templateRows="40px 1fr">
-        <GridItem rowStart={1} colSpan={2} id="jsonTitle">
+      <Grid templateColumns="90% 10%" templateRows="40px 1fr">
+        <GridItem rowStart={1} colSpan={2} id="myStoryTitle">
           <Flex width="100%" height="100%" alignItems={"center"}>
             <div>
               <span className="jsonLogo">{"{i} "}</span>MyStory.json
             </div>
           </Flex>
         </GridItem>
-        <GridItem rowStart={2} colStart={1} className="bodySectionBody">
+        <GridItem
+          rowStart={2}
+          colStart={1}
+          className="bodySectionBody"
+          height={codeBodyHeight}
+        >
           <HStack alignItems={"flex-start"}>
-            <Box height={codeBodyHeight} className="listNumbersContainer">
+            <Box className="listNumbersContainer">
               <LineNumbers />
             </Box>
             <div ref={codeBodyRef}>
