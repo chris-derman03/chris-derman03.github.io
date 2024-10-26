47326f7986eea78387e28bb62aea2ca8ddf5062f
Christian DerManuelian <christian.dermanuelian@gmail.com>
2024-10-26 03:59:28 -0700

    Refactored the way images are loaded to avoid loading all images for each icon list

--- src/components/Projects/SkillsIconList.tsx
+++ src/components/Projects/SkillsIconList.tsx
@@ -1,74 +1,39 @@
-import { HStack, Icon, Tooltip, Image } from "@chakra-ui/react";
-import { IconType } from "react-icons";
-
-import PythonIcon from "/public/img/python_logo.png";
-import RegressionIcon from "/public/img/Regression.webp";
-import ViteLogo from "/public/img/vite.webp";
-import TypeScriptLogo from "/public/img/typescript.webp";
-import HTMLLogo from "/public/img/html.webp";
-import JSLogo from "/public/img/js.webp";
-import CSSLogo from "/public/img/css.webp";
-import SKLearnLogo from "/public/img/sklearn.webp";
-import AWSLogo from "/public/img/aws.webp";
-import COMPARCHIcon from "/public/img/comp_arch.webp";
-import NNIcon from "/public/img/nn.webp";
-import PandasIcon from "/public/img/pandas.webp";
-import DeeoNNIcon from "/public/img/deepNN.webp";
-import HypTuneIcon from "/public/img/hypTune.webp";
-import AGIIcon from "/public/img/agi.webp";
-import LLMIcon from "/public/img/transformer.webp";
-import PromptEngIcon from "/public/img/prompt_eng.webp";
-import NumpyLogo from "/public/img/numpy.webp";
-import TensorflowLogo from "/public/img/tensorflow.webp";
-import KerasLogo from "/public/img/keras.webp";
-import CNNIcon from "/public/img/cnn.webp";
-import MLIcon from "/public/img/ml.webp";
-import JupyterLogo from "/public/img/jupyter.webp";
-import ReactLogo from "/public/img/react.webp";
-import PYGameLogo from "/public/img/pygame.webp";
-import PytorchLogo from "/public/img/pytorch.webp";
-import FeatEngIcon from "/public/img/feat_eng.webp";
-import MatplotlibLogo from "/public/img/matplotlib.webp";
-import EventLoopIcon from "/public/img/event_loop.webp";
-import CircuitIcon from "/public/img/circuit.webp";
-import RaspIcon from "/public/img/rasp.webp";
-import LinuxLogo from "/public/img/linux.webp";
-import StatsIcon from "/public/img/stats.webp";
+import { HStack, Tooltip, Image } from "@chakra-ui/react";
 
 const iconMap: { [key: string]: string } = {
-  "Neural Networks": NNIcon,
-  "Deep Learning": DeeoNNIcon,
-  "Hyperparameter Tuning": HypTuneIcon,
-  AGI: AGIIcon,
-  "Prompt Engineering": PromptEngIcon,
-  LLMs: LLMIcon,
-  Python: PythonIcon,
-  pandas: PandasIcon,
-  PyTorch: PytorchLogo,
-  sklearn: SKLearnLogo,
-  "React.js": ReactLogo,
-  Vite: ViteLogo,
-  TypeScript: TypeScriptLogo,
-  JavaScript: JSLogo,
-  HTML: HTMLLogo,
-  CSS: CSSLogo,
-  "Computer Architecture": COMPARCHIcon,
-  Jupyter: JupyterLogo,
-  "Feature Engineering": FeatEngIcon,
-  AWS: AWSLogo,
-  NumPy: NumpyLogo,
-  Matplotlib: MatplotlibLogo,
-  TensorFlow: TensorflowLogo,
-  Keras: KerasLogo,
-  "Convolutional Neural Network": CNNIcon,
-  "Machine Learning": MLIcon,
-  Pygame: PYGameLogo,
-  "Event Loops": EventLoopIcon,
-  Circuits: CircuitIcon,
-  "Raspberry Pi": RaspIcon,
-  Linux: LinuxLogo,
-  "Linear Regression": RegressionIcon,
-  Statistics: StatsIcon,
+  "Neural Networks": "/public/img/nn.webp",
+  "Deep Learning": "/public/img/deepNN.webp",
+  "Hyperparameter Tuning": "/public/img/hypTune.webp",
+  AGI: "/public/img/agi.webp",
+  "Prompt Engineering": "/public/img/prompt_eng.webp",
+  LLMs: "/public/img/transformer.webp",
+  Python: "/public/img/python_logo.png",
+  pandas: "/public/img/pandas.webp",
+  PyTorch: "/public/img/pytorch.webp",
+  sklearn: "/public/img/sklearn.webp",
+  "React.js": "/public/img/react.webp",
+  Vite: "/public/img/vite.webp",
+  TypeScript: "/public/img/typescript.webp",
+  JavaScript: "/public/img/js.webp",
+  HTML: "/public/img/html.webp",
+  CSS: "/public/img/css.webp",
+  "Computer Architecture": "/public/img/comp_arch.webp",
+  Jupyter: "/public/img/jupyter.webp",
+  "Feature Engineering": "/public/img/feat_eng.webp",
+  AWS: "/public/img/aws.webp",
+  NumPy: "/public/img/numpy.webp",
+  Matplotlib: "/public/img/matplotlib.webp",
+  TensorFlow: "/public/img/tensorflow.webp",
+  Keras: "/public/img/keras.webp",
+  "Convolutional Neural Network": "/public/img/cnn.webp",
+  "Machine Learning": "/public/img/ml.webp",
+  Pygame: "/public/img/pygame.webp",
+  "Event Loops": "/public/img/event_loop.webp",
+  Circuits: "/public/img/circuit.webp",
+  "Raspberry Pi": "/public/img/rasp.webp",
+  Linux: "/public/img/linux.webp",
+  "Linear Regression": "/public/img/Regression.webp",
+  Statistics: "/public/img/stats.webp",
 };
 
 interface Props {
